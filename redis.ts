45c5e70367a238293584c23f311c0dbfe1c88498
Audi <audi.previo98@gmail.com>
2023-10-13 23:46:55 +0700

    commit redis update

--- redis.ts
+++ redis.ts
@@ -1,5 +1,9 @@
 import Redis from 'ioredis';
 
-const redis = new Redis();
+const redisUrl = process.env.REDIS_URL;
+if (!redisUrl) {
+    throw new Error('REDIS_URL is not defined')
+}
+const redis = new Redis(redisUrl);
 
 export default redis;
